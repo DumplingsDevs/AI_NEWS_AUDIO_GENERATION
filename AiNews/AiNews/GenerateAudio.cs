d3c0f668dd0c96c97f90103a82b4ac74c4f4f659
KmLPro <kamil.bytner@gmail.com>
2023-12-01 00:42:40 +0100

    Add new azure function project with First Http Trigger

---
+++ AiNews/AiNews/GenerateAudio.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using System.Net;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+
+namespace AiNews;
+
+public static class GenerateAudio
+{
+    [Function("GenerateAudio")]
+    public static HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req,
+        FunctionContext executionContext)
+    {
+        var logger = executionContext.GetLogger("GenerateAudio");
+        logger.LogInformation("C# HTTP trigger function processed a request.");
+
+        var response = req.CreateResponse(HttpStatusCode.OK);
+        response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+
+        response.WriteString("Welcome to Azure Functions!");
+
+        return response;
+    }
+}
