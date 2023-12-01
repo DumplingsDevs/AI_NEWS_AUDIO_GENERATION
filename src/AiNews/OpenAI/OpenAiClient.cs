31866ef2d51e433dab2965a9caee329ad21da70c
Karasini <Karasini@users.noreply.github.com>
2023-12-01 14:03:25 +0100

    Add audio generation with open ai

---
+++ src/AiNews/OpenAI/OpenAiClient.cs
@@ -0,0 +1,24 @@
+using System.Net.Http.Json;
+
+namespace AiNews.OpenAI;
+
+internal static class OpenAiClient
+{
+    public static async Task<byte[]> GetAudio(HttpClient httpClient, string input)
+    {
+        var requestData = new
+        {
+            model = "tts-1-hd",
+            voice = "nova",
+            input,
+        };
+
+        using var response = await httpClient.PostAsJsonAsync("/v1/audio/speech", requestData);
+        if (response.IsSuccessStatusCode)
+        {
+            return await response.Content.ReadAsByteArrayAsync();
+        }
+
+        throw new Exception($"ErrorCode from OpenAI: {response.StatusCode}");
+    }
+}
