4b2061066fe8e5ca8d6b746fe5d0c925edd3efa0
Karasini <Karasini@users.noreply.github.com>
2024-01-12 16:45:30 +0100

    TriggerWarmup function

---
+++ src/AiNews/TriggerWarmup.cs
@@ -0,0 +1,17 @@
+using AiNews.Dtos;
+using AiNews.Exceptions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+
+namespace AiNews;
+
+public class TriggerWarmup
+{
+    [Function("TriggerWarmup")]
+    public IResult Run([HttpTrigger(AuthorizationLevel.Function, "get")] HttpRequest req)
+    {
+        return Results.Ok("Warmed up");
+    }
+    
+}
